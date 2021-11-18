e32ac8fe50afeee499d6383e69409d4e3c28e2af
AlbertoAliaga <100363863@alumnos.uc3m.es>
2021-11-18 13:12:43 +0100

    Input reading complete

--- csp.py
+++ csp.py
@@ -2,30 +2,43 @@
 
 import sys
 import os
+from constraint import *
 
 ######################## Input reading ##################################
 
 path = sys.argv[1]
-map = sys.argv[2]
-container = sys.argv[3]
+cells = sys.argv[2]
+containers = sys.argv[3]
 
 os.chdir(path)
-map_file_size = os.stat(str(path)+"/"+str(map)).st_size
-container_file_size = os.stat(str(path)+"/"+str(container)).st_size
+cells_file_size = os.stat(str(path)+"/"+str(cells)).st_size
+containers_file_size = os.stat(str(path)+"/"+str(containers)).st_size
 
 
-map = open(map, "r")
-container = open(container, "r")
+cells = open(cells, "r")
+containers = open(containers, "r")
 
-map = map.read(map_file_size)
-container = container.read(container_file_size)
+cells = cells.read(cells_file_size)
+containers = containers.read(containers_file_size)
 
-map = map.split("\n")
-container = container.split("\n")
+cells = cells.split("\n")
+containers = containers.split("\n")
 
-print(map)
-print("\t")
-print(container)
-print("\t")
+#Cells become a reversed tuple matrix, since container height goes upwards
+for i in range(0, len(cells)):
+    cells[i] = tuple(cells[i].split())
+cells.reverse()
+cells = tuple(cells)
 
-#########################  ##################################
+#Containers are represented as a tuple of tuples
+for i in range(0, len(containers)):
+    containers[i] = tuple(containers[i].split())
+containers = tuple(containers)
+
+print(cells, "\n")
+#print(containers, "\n")
+
+######################### Variable Assignation ##########################
+
+problem = Problem()
+
