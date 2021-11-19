d5609b3a13d4aebb8b91fc2f69b39cc6f8951971
Rubén <100363901@alumnos.uc3m.es>
2021-11-19 10:45:46 +0100

    Added variable creation

--- csp.py
+++ csp.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python
 
+
 import sys
 import os
+from constraint import *
+
 
 ######################## Input reading ##################################
+# para correr el programa en el PC de RBN: python3 csp.py /home/rbn/pyCharmProjects/Heuristica/lab-2/Heuristics/P2 Input-cells.txt Input-cont.txt
 
 path = sys.argv[1]
 map = sys.argv[2]
@@ -23,9 +27,39 @@ container = container.read(container_file_size)
 map = map.split("\n")
 container = container.split("\n")
 
+'''
 print(map)
-print("\t")
+print()
 print(container)
-print("\t")
+print()'''
 
 #########################  ##################################
+
+'''
+    Cada input de Input-cont.txt es una variable. Los dominios son si necesita una posición con
+    energía o no (si es R la necesita, si es S no tiene por qué) y a qué puerto van
+'''
+
+problem = Problem()
+
+# Dominio 0 = N     Dominio 1 = E       Destino X = X
+
+# En este bucle se sacan las variables de contenedores y se asignan los dominios
+for i in container:
+    unit_container = i.split()
+    id = unit_container[0]
+    type = unit_container[1]
+    destination = unit_container[2]
+    #print("id = ", id, "\ttype = ", type, "\tdest = ", destination)
+
+
+    if(type == "R"):
+        print("Container w/ R's id = ", id)
+        problem.addVariable(id, ['N', destination])
+    elif (type == "S"):
+        print("Container w/ S's id = ", id)
+        problem.addVariable(id, ['N','E', destination])
+    else:
+        print("Input error. ", sys.argv[3], " contains wrong data.")
+
+
